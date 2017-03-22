4e00da94cf2d6d48317f61cc75e702c6d26b2f0a
Henry J. Wylde <me@hjwylde.com>
2017-03-23 10:42:26 +1300

    Remove unused methods

--- app/src/main/java/com/hjwylde/rivers/ui/activities/BaseActivity.java
+++ app/src/main/java/com/hjwylde/rivers/ui/activities/BaseActivity.java
@@ -4,19 +4,12 @@ import android.content.Context;
 import android.support.annotation.IdRes;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 public abstract class BaseActivity extends AppCompatActivity {
-    @NonNull
-    public ImageView findImageViewById(@IdRes int id) {
-        return findTById(id);
-    }
-
     @NonNull
     public <T extends View> T findTById(@IdRes int id) {
         return (T) findViewById(id);
@@ -24,11 +17,6 @@ public abstract class BaseActivity extends AppCompatActivity {
 
     @NonNull
     public TextView findTextViewById(@IdRes int id) {
-        return findTById(id);
-    }
-
-    @NonNull
-    public Toolbar findToolbarById(@IdRes int id) {
         return findTById(id);
     }
 
