25ceb975a292fcba1381d1bff3a570c6b9b7b4a8
Henry J. Wylde <me@hjwylde.com>
2017-04-21 10:14:25 +1200

    (#13) pull out soft input hide to util class

---
+++ app/src/main/java/com/hjwylde/rivers/ui/util/SoftInput.java
@@ -0,0 +1,19 @@
+package com.hjwylde.rivers.ui.util;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+
+public final class SoftInput {
+    private SoftInput() {
+    }
+
+    public static void hide(Activity activity) {
+        View view = activity.getCurrentFocus();
+        if (view != null) {
+            InputMethodManager imm = (InputMethodManager) activity.getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
+    }
+}
