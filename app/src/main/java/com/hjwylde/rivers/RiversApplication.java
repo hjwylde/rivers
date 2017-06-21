2a808387f14dbd665c9734795add6b81ea779319
Henry J. Wylde <me@hjwylde.com>
2017-06-22 10:11:43 +1200

    (#25) replicate to the sync gateway

--- app/src/main/java/com/hjwylde/rivers/RiversApplication.java
+++ app/src/main/java/com/hjwylde/rivers/RiversApplication.java
@@ -2,20 +2,26 @@ package com.hjwylde.rivers;
 
 import android.app.Application;
 import android.support.annotation.NonNull;
+import android.util.Log;
 
 import com.couchbase.lite.CouchbaseLiteException;
 import com.couchbase.lite.Database;
 import com.couchbase.lite.DatabaseOptions;
 import com.couchbase.lite.Manager;
 import com.couchbase.lite.android.AndroidContext;
+import com.couchbase.lite.replicator.Replication;
 import com.hjwylde.rivers.db.services.CouchbaseRepository;
 import com.hjwylde.rivers.services.Repository;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
 
 public class RiversApplication extends Application {
+    private static final String TAG = RiversApplication.class.getSimpleName();
+
     private static Database mDatabase;
 
     private static Repository mRepository;
@@ -32,6 +38,7 @@ public class RiversApplication extends Application {
         setUpCalligraphy();
 
         setUpDatabase();
+        setUpDatabaseReplicators();
 
         setUpRepository();
     }
@@ -53,8 +60,26 @@ public class RiversApplication extends Application {
 
             mDatabase = manager.openDatabase("rivers", options);
         } catch (IOException | CouchbaseLiteException e) {
-            e.printStackTrace();
+            Log.e(TAG, e.getMessage(), e);
+
             // TODO (hjw): panic
+        }
+    }
+
+    private void setUpDatabaseReplicators() {
+        try {
+            URL url = new URL("http://45.32.247.9:4984/db");
+
+            Replication push = mDatabase.createPushReplication(url);
+            Replication pull = mDatabase.createPullReplication(url);
+
+            push.setContinuous(true);
+            pull.setContinuous(true);
+
+            push.start();
+            pull.start();
+        } catch (MalformedURLException e) {
+            Log.e(TAG, e.getMessage(), e);
         }
     }
 
